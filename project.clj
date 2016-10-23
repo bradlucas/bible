9d49cfc9e20327de12bf30331d2877b883b43d1d
Brad Lucas <brad@beaconhill.com>
2016-10-23 09:09:54 -0400

    clojars group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bible "1.0.0"
+(defproject com.bradlucas/bible "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
